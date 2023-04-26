7e2ee55f6598fc68e3a7167432d3ea411940db53
oeMicky <mvizconde@msn.com>
2023-04-26 10:57:49 -0500

    2023-04-26-III

--- src/components/venta/tablaVentas.tsx
+++ src/components/venta/tablaVentas.tsx
@@ -93,8 +93,9 @@ export default component$((props: { buscarVentas: number; parameBusqueda: { idGr
     const abortController = new AbortController();
     cleanup(() => abortController.abort('cleanup'));
 
-    console.log('FETCH->: ', `http://localhost:4000/api/venta/obtenerVentasPorFechas`);
-    const res = await fetch(`http://localhost:4000/api/venta/obtenerVentasPorFechas`, {
+    // console.log('FETCH->: ', `http://localhost:4000/api/venta/obtenerVentasPorFechas`);
+    // const res = await fetch(`http://localhost:4000/api/venta/obtenerVentasPorFechas`, {
+    const res = await fetch(`https://backendalmacen-production.up.railway.app/api/venta/obtenerVentasPorFechas`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
