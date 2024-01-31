0621cebcd1508a14630166ed60dff5dc95ef023b
oeMicky <mvizconde@msn.com>
2024-01-31 11:19:34 -0500

    venta comra 31ENE 11.19

--- src/routes/(almacen)/compra/index.tsx
+++ src/routes/(almacen)/compra/index.tsx
@@ -195,7 +195,7 @@ export default component$(() => {
         <ElSelect
           id={'se_periodo_COMPRA'}
           // valorSeleccionado={definicion_CTX_COMPRA.documentoCompra}
-          estilos={{ width: '108px', marginLeft: '5px' }}
+          estilos={{ width: '114px', marginLeft: '5px' }}
           registros={losPeriodosCargados.value}
           registroID={'_id'}
           registroTEXT={'periodo'}
